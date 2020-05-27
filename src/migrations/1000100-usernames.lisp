7037b5910af7a459cbeb4f6fabcfcb94a4f9e8bf
Wimpie Nortje <wimpie@selectiveshare.com>
2020-05-27 20:37:09 +0200

    Rename migration file.

--- src/migrations/1000100-usernames.lisp
+++ src/migrations/1000100-usernames.lisp
@@ -6,6 +6,8 @@
       "create usernames"
     :execute "CREATE TABLE usernames ( 
                  key SERIAL UNIQUE, 
+                 created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
+                 modified_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
                  username TEXT UNIQUE NOT NULL)"
     :revert "DROP TABLE usernames"))
  
